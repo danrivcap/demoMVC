cfaecd31a106d540538a00bfdd58c401e4693266
dri <dri@novatec-gmbh.de>
2025-01-21 20:01:53 +0100

    Add controller layer.

---
+++ src/main/java/de/novatec/springworkshop/demomvc/students/controller/StudentResource.java
@@ -0,0 +1,40 @@
+package de.novatec.springworkshop.demomvc.students.controller;
+
+import de.novatec.springworkshop.demomvc.students.dto.StudentDTO;
+import de.novatec.springworkshop.demomvc.students.service.StudentService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/university")
+public class StudentResource {
+    private final StudentService studentService;
+
+    public StudentResource(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<StudentDTO> findAll(){
+        return studentService.findAll();
+    }
+
+    @GetMapping("/student/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public StudentDTO findStudentByName(@PathVariable String name) {
+        return studentService.find(name);
+    }
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStudent(@RequestBody StudentDTO studentDTO) {
+        studentService.save(studentDTO);
+    }
+
+
+}
