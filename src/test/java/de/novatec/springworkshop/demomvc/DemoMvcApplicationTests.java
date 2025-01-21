87ddc5e4a9254833de210c8ef3ec2d1c5a969bf5
dri <dri@novatec-gmbh.de>
2025-01-21 19:03:30 +0100

    Initial commit

---
+++ src/test/java/de/novatec/springworkshop/demomvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.novatec.springworkshop.demomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
