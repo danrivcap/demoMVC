868778ee8bea398352816b0a6f08a215d6a4b28d
dri <dri@novatec-gmbh.de>
2025-01-21 19:24:20 +0100

    Student Repository, first approach

---
+++ src/main/java/de/novatec/springworkshop/demomvc/students/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package de.novatec.springworkshop.demomvc.students.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import de.novatec.springworkshop.demomvc.students.entity.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+}
